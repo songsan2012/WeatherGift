83aadf523991c7fb85c0c54983488103328282cc
Song San <songsan2012@gmail.com>
2022-02-26 19:52:53 -0800

    Add Weather Detail Subclass and get temperature, description, etc. from openweather API JSON

--- WeatherGift/LocationDetailViewController.swift
+++ WeatherGift/LocationDetailViewController.swift
@@ -10,17 +10,14 @@ import UIKit
 
 class LocationDetailViewController: UIViewController {
     @IBOutlet weak var dateLabel: UILabel!
-    
     @IBOutlet weak var placeLabel: UILabel!
-    
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var summaryLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
-    
-    
     @IBOutlet weak var pageControl: UIPageControl!
     
-    var weatherLocation: WeatherLocation!
+//    var weatherLocation: WeatherLocation!
+    var weatherDetail: WeatherDetail!
     var locationIndex = 0
     
     override func viewDidLoad() {
@@ -38,17 +35,23 @@ class LocationDetailViewController: UIViewController {
     func updateUserInterface()
     {
         let pageViewController = UIApplication.shared.windows.first!.rootViewController as! PageViewController
-        weatherLocation = pageViewController.weatherLocations[locationIndex]
+        let weatherLocation = pageViewController.weatherLocations[locationIndex]
+        weatherDetail = WeatherDetail(name: weatherLocation.name, latitude: weatherLocation.latitude, longitude: weatherLocation.longitude)
         
-        dateLabel.text = ""
-        placeLabel.text = weatherLocation.name
-        temperatureLabel.text = "--°"
-        summaryLabel.text = ""
+
         
         pageControl.numberOfPages = pageViewController.weatherLocations.count
         pageControl.currentPage = locationIndex
         
-        weatherLocation.getData()
+        weatherDetail.getData {
+            DispatchQueue.main.async {
+                self.dateLabel.text = self.weatherDetail.timezone
+                self.placeLabel.text = self.weatherDetail.name
+                self.temperatureLabel.text = "\(self.weatherDetail.temperature)°"
+                self.summaryLabel.text = self.weatherDetail.summary
+            }
+
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
